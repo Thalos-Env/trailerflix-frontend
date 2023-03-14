f687939ec8b2170f3809d5530102efe064660a7b
1carlosrenan <carlosrenan96@gmail.com>
2023-03-14 08:34:33 -0300

    test: adding todo to config jest file

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,5 +2,7 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+
+// TODO: configurar corretamente o jest.config.js
 import '@testing-library/jest-dom'
 import 'jest-styled-components'
