2975f0645e9155847ee3e099ca2724d55d7d39f6
1CarlosRenan <carlosrenan96@gmail.com>
2023-04-11 22:20:11 -0300

    feat: adding carousel title

---
+++ src/components/CarouselTitle/CarouselTitle.stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react'
+import CarouselTitle from '.'
+
+export default {
+  title: 'CarouselTitle',
+  component: CarouselTitle,
+} as Meta
+
+export const Default: Story = () => <CarouselTitle />
---
+++ src/components/CarouselTitle/index.tsx
@@ -0,0 +1,5 @@
+import * as S from './styles'
+
+const CarouselTitle = () => <S.Wrapper>CarouselTitle</S.Wrapper>
+
+export default CarouselTitle
