f6ee3ca4d19d4633bb22ef6f0da9623579927022
carlos renan <carlosrenan96@gmail.com>
2023-04-29 23:01:14 -0300

    Revert "feat: add hero base"
    This reverts commit 618f6b7c0a64fa3df22d7c32e013717d44911d25.

--- src/components/Hero/Hero.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react'
-import Hero from '.'
-
-export default {
-  title: 'Hero',
-  component: Hero,
-} as Meta
-
-export const Default: Story = () => <Hero />
--- src/components/Hero/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as S from './styles'
-
-const Hero = () => (
-  <S.Wrapper>
-    <h1>Hero</h1>
-  </S.Wrapper>
-)
-
-export default Hero
