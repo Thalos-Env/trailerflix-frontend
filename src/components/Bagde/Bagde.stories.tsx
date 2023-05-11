84493004b1474551d23267faf011ad0fe6dc86d9
carlos renan <carlosrenan96@gmail.com>
2023-05-10 22:39:34 -0300

    feat: add bagde base

---
+++ src/components/Bagde/Bagde.stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react'
+import Bagde from '.'
+
+export default {
+  title: 'Bagde',
+  component: Bagde,
+} as Meta
+
+export const Default: Story = () => <Bagde />
---
+++ src/components/Bagde/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+const Bagde = () => (
+  <S.Wrapper>
+    <h1>Bagde</h1>
+  </S.Wrapper>
+)
+
+export default Bagde
