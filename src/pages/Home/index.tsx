7d6f876a6d43646878a1654d843da79be6876bd7
carlos renan <carlosrenan96@gmail.com>
2023-05-20 23:10:21 -0300

    feat: add home base

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,39 @@
+import Carousel from '../../components/Carousel'
+import Footer from '../../components/Footer'
+import Hero from '../../components/Hero'
+import MenuHeader from '../../components/MenuHeader'
+import MovieCard from '../../components/MovieCard'
+import * as S from './styles'
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <S.Wrapper>
+      <MenuHeader />
+      <Hero
+        title='Guardiões da Galáxia'
+        description='Peter Quill deve reunir sua equipe para defender o universo e proteger um dos seus. Se a missão não for totalmente bem-sucedida, isso pode levar ao fim dos Guardiões.'
+        backgroundImageUrl='/assets/images/cartaz.jpg'
+        onClickAdd={() => console.log('Adicionar')}
+        onClickInfo={() => console.log('Info')}
+        onClickPlay={() => console.log('Play')}
+      />
+      <Carousel title='carousel'>
+        {Array.from(Array(20).keys()).map((_, index) => (
+          <MovieCard key={index} />
+        ))}
+      </Carousel>
+      <Carousel title='carousel'>
+        {Array.from(Array(20).keys()).map((_, index) => (
+          <MovieCard key={index} />
+        ))}
+      </Carousel>
+      <Carousel title='carousel'>
+        {Array.from(Array(20).keys()).map((_, index) => (
+          <MovieCard key={index} />
+        ))}
+      </Carousel>
+      <Footer />
+    </S.Wrapper>
   )
 }
 
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,10 @@
+import styled, { css } from 'styled-components'
+
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    height: 100%;
+    background-color: ${theme.colors.mainBg};
+
+    padding: ${theme.spacings.xsmall} ${theme.spacings.xlarge};
+  `}
+`
