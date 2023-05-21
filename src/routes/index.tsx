890b504a1742c990f462a874104c8714ece594cc
carlos renan <carlosrenan96@gmail.com>
2023-05-20 22:39:22 -0300

    feat: home route config

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,10 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
+import Home from '../pages/Home'
 
 const AppRoutes = () => {
   return (
     <Routes>
+      <Route path='/' element={<Home />} />
       <Route path='/pagina-inicial' element={<p>Página Inicial</p>} />
       <Route path='/teste' element={<p>Página teste</p>} />
       <Route path='*' element={<Navigate to='/pagina-inicial' />} />
